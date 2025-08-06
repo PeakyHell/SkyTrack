87fb276d6b9d6a4a294faacf02d3ef799e96b313
PeakyHell <louis.vanimpe@student.uclouvain.be>
2025-08-06 14:41:34 +0200

    Actually render the filled box

--- src/main/java/com/peakyhell/skytrack/render/RenderUtils.java
+++ src/main/java/com/peakyhell/skytrack/render/RenderUtils.java
@@ -2,14 +2,14 @@ package com.peakyhell.skytrack.render;
 
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormats;
-import net.minecraft.client.render.VertexRendering;
+
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+
+import java.nio.Buffer;
 
 public class RenderUtils {
 
@@ -37,6 +37,11 @@ public class RenderUtils {
         BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
 
         VertexRendering.drawFilledBox(matrixStack, buffer, box.minX, box.minY, box.minZ, box.maxX, box.maxY, box.maxZ, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+        BuiltBuffer builtBuffer = buffer.end();
+
+        RenderLayer renderLayer = rgba[3] > 1.0f ? RenderLayer.getCutout() : RenderLayer.getSolid();
+        renderLayer.draw(builtBuffer);
 
         matrixStack.pop();
     }
