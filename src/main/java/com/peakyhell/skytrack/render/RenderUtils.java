8bab00c3c9039034cce2ef96c7cae2a7ee4feeca
PeakyHell <louis.vanimpe@student.uclouvain.be>
2025-08-07 20:19:12 +0200

    Simplify Box rendering methods

--- src/main/java/com/peakyhell/skytrack/render/RenderUtils.java
+++ src/main/java/com/peakyhell/skytrack/render/RenderUtils.java
@@ -18,25 +18,7 @@ public class RenderUtils {
      * @param rgba The color parameters
      */
     public static void renderOutlinedBox(WorldRenderContext context, Box box, float[] rgba) {
-        MatrixStack matrixStack = context.matrixStack();
-        Vec3d camera = context.camera().getPos();
-
-        if (matrixStack == null || camera == null) return;
-
-        matrixStack.push();
-        matrixStack.translate(-camera.x, -camera.y, -camera.z);
-
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.LINES, VertexFormats.POSITION_COLOR);
-
-        drawOutlinedBox(matrixStack.peek().getPositionMatrix(), buffer, box, rgba);
-
-        BuiltBuffer builtBuffer = buffer.end();
-
-        RenderLayer renderLayer = RenderLayer.getDebugQuads();
-        renderLayer.draw(builtBuffer);
-
-        matrixStack.pop();
+        renderBox(context, box, rgba, false);
     }
 
     /**
@@ -46,6 +28,18 @@ public class RenderUtils {
      * @param rgba The color parameters
      */
     public static void renderFilledBox(WorldRenderContext context, Box box, float[] rgba) {
+        renderBox(context, box, rgba, true);
+    }
+
+    /**
+     * Renders a filled or outlined box at given coordinates with the given color
+     * @param context The WorldRenderContext
+     * @param box The box coordinates
+     * @param rgba The color parameters
+     * @param filled - True if the box must be filled,
+     *               - False if the box must be outlined
+     */
+    public static void renderBox(WorldRenderContext context, Box box, float[] rgba, boolean filled) {
         MatrixStack matrixStack = context.matrixStack();
         Vec3d camera = context.camera().getPos();
 
@@ -57,11 +51,11 @@ public class RenderUtils {
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
 
-        drawFilledBox(matrixStack.peek().getPositionMatrix(), buffer, box, rgba);
+        drawBox(matrixStack.peek().getPositionMatrix(), buffer, box, rgba);
 
         BuiltBuffer builtBuffer = buffer.end();
 
-        RenderLayer renderLayer = RenderLayer.getDebugQuads();
+        RenderLayer renderLayer = filled ? RenderLayer.getDebugQuads() : RenderLayer.getDebugLineStrip(1.0f);
         renderLayer.draw(builtBuffer);
 
         matrixStack.pop();
@@ -74,67 +68,7 @@ public class RenderUtils {
      * @param box The box coordinates
      * @param rgba The color parameters
      */
-    public static void drawOutlinedBox(Matrix4f matrix, BufferBuilder buffer, Box box, float[]rgba) {
-        float minX = (float) box.minX;
-        float maxX = (float) box.maxX;
-        float minY = (float) box.minY;
-        float maxY = (float) box.maxY;
-        float minZ = (float) box.minZ;
-        float maxZ = (float) box.maxZ;
-
-        float r = rgba[0];
-        float g = rgba[1];
-        float b = rgba[2];
-        float a = rgba[3];
-
-        // Bottom
-        buffer.vertex(matrix, minX, minY, minZ).color(r, g, b, a);
-        buffer.vertex(matrix, minX, minY, maxZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, minX, minY, maxZ).color(r, g, b, a);
-        buffer.vertex(matrix, maxX, minY, maxZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, maxX, minY, maxZ).color(r, g, b, a);
-        buffer.vertex(matrix, maxX, minY, minZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, maxX, minY, minZ).color(r, g, b, a);
-        buffer.vertex(matrix, minX, minY, minZ).color(r, g, b, a);
-
-        // Top
-        buffer.vertex(matrix, minX, maxY, minZ).color(r, g, b, a);
-        buffer.vertex(matrix, minX, maxY, maxZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, minX, maxY, maxZ).color(r, g, b, a);
-        buffer.vertex(matrix, maxX, maxY, maxZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, maxX, maxY, maxZ).color(r, g, b, a);
-        buffer.vertex(matrix, maxX, maxY, minZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, maxX, maxY, minZ).color(r, g, b, a);
-        buffer.vertex(matrix, minX, maxY, minZ).color(r, g, b, a);
-
-        // Vertical
-        buffer.vertex(matrix, minX, minY, minZ).color(r, g, b, a);
-        buffer.vertex(matrix, minX, maxY, minZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, minX, minY, maxZ).color(r, g, b, a);
-        buffer.vertex(matrix, minX, maxY, maxZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, maxX, minY, maxZ).color(r, g, b, a);
-        buffer.vertex(matrix, maxX, maxY, maxZ).color(r, g, b, a);
-
-        buffer.vertex(matrix, maxX, minY, minZ).color(r, g, b, a);
-        buffer.vertex(matrix, maxX, maxY, minZ).color(r, g, b, a);
-    }
-
-    /**
-     * Draw an outlined box with the given dimensions and color in a buffer to be ready to be rendered
-     * @param matrix The matrix
-     * @param buffer The buffer
-     * @param box The box coordinates
-     * @param rgba The color parameters
-     */
-    public static void drawFilledBox(Matrix4f matrix, BufferBuilder buffer, Box box, float[] rgba) {
+    public static void drawBox(Matrix4f matrix, BufferBuilder buffer, Box box, float[] rgba) {
         float minX = (float) box.minX;
         float maxX = (float) box.maxX;
         float minY = (float) box.minY;
